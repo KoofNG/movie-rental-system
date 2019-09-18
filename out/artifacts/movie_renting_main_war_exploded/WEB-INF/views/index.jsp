78ee82b59a25ed89d2cb3fcaf5b0c46eac8e5de0
KoofNG <israelakpan296@gmail.com>
2019-09-18 14:00:12 +0100

    Successfully Setup Spring 5 MVC Project

---
+++ out/artifacts/movie_renting_main_war_exploded/WEB-INF/views/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: israel
+  Date: 18/09/2019
+  Time: 8:20 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+
+  <h4>God WIn oooooo</h4>
+  </body>
+</html>
---
+++ src/main/java/movies/Controllers/HomePage.java
@@ -0,0 +1,15 @@
+package movies.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePage {
+
+    // Return the Index page
+    @GetMapping("/")
+    public String showIndexPage() {
+        System.out.println("You are in me -> Show index page");
+        return "index";
+    }
+}
